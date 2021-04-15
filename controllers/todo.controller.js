4f3c37faab9132cec2cc07236bd37bfeb39b1d16
Cloudbop <boppincolin@gmail.com>
2021-04-15 17:50:10 +0100

    +cmnt

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -3,5 +3,6 @@ const TodoModel = require('../model/todo.model')
 // 
 exports.createTodo = (req,res,next) => {
   const createdModel = TodoModel.create(req.body);
+  // 201 is for data created on server correctly
   res.status(201).json(createdModel);
 }
