786a1d8c69359be4f0b1ed7f709689ad4276fde9
Cloudbop <boppincolin@gmail.com>
2021-04-15 17:21:43 +0100

    fail: should invoke TodoModel.create()

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -1,7 +1,18 @@
 const TodoController = require("../../controllers/todo.controller");
+const TodoModel = require("../../model/todo.model");
 
+// mock implementation
+TodoModel.create = jest.fn();
+//
 describe("TodoController.createTodo", () => {
   it("should have a createTodo function", () => {
     expect(typeof TodoController.createTodo).toBe("function");
   });
+  it("should invoke TodoModel.create()", () => {
+    
+    TodoController.createTodo();
+    expect(TodoModel.create).toBeCalled();
+  });
+
+
 });
