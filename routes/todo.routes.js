3b96f298155e667786b2a4d654fd92cb3662e6d2
Cloudbop <boppincolin@gmail.com>
2021-04-16 00:06:30 +0100

    fix implementation

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -3,6 +3,7 @@ const todoController = require('../controllers/todo.controller')
 const router = express.Router()
 
 router.post("/", todoController.createTodo)
+router.get("/", todoController.getTodos)
 
 
 module.exports = router
