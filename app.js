20a0750f6e85598087c0261b04a94f3feda51226
Cloudbop <boppincolin@gmail.com>
2021-04-15 16:05:13 +0100

    +bp express setup

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  // || res.send for templating
+  res.json("Hello world!"); // for REST api 
+});
+
+app.listen(3000, () => {
+  console.log("Server is now running!");
+});
