6908483e1967d25278ed45ff1f898975d61c0e89
Cloudbop <boppincolin@gmail.com>
2021-04-15 17:48:43 +0100

    pass: should return json body response

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -2,7 +2,6 @@ const TodoModel = require('../model/todo.model')
 
 // 
 exports.createTodo = (req,res,next) => {
-  TodoModel.create(req.body)
-  res.status(201).json();
-
+  const createdModel = TodoModel.create(req.body);
+  res.status(201).json(createdModel);
 }
