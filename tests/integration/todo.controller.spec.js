29664f9083325f08a7839dd0893f3cd7ee04dbe6
Cloudbop <boppincolin@gmail.com>
2021-04-15 21:55:03 +0100

    fail: Implementation error, noDb

--- tests/integration/todo.controller.spec.js
+++ tests/integration/todo.controller.spec.js
@@ -3,14 +3,13 @@ const app = require('../../app');
 const newTodo = require('../mock-data/newTodo.json');
 const endpointUrl = "/todos/"
 
-describe(endpointUrl, ()=>{
-  it("POST "+endpointUrl , async () => {
-    const response = await request(app)
-    .post(endpointUrl)
-    .send(newTodo);
-
+describe(`${endpointUrl}`, ()=>{
+  it(`POST ${endpointUrl}`, async () => {
+    //
+    const response = await request(app).post(endpointUrl).send(newTodo);
+    console.log(`response`, JSON.stringify(response.body))
     expect(response.statusCode).toBe(201);
-    expect(response.body.title).toBe(newTodo.tite);
+    expect(response.body.title).toBe(newTodo.title);
     expect(response.body.done).toBe(newTodo.done);
   })
 })
