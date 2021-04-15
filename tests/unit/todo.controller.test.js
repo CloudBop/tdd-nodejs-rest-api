065d031ca4edd497e7b8323d80af9c20c23ccbf2
Cloudbop <boppincolin@gmail.com>
2021-04-15 23:56:42 +0100

    fail:should handle error in getTodos

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -36,6 +36,14 @@ describe("TodoController.getTodos", () => {
     expect(res._isEndCalled()).toBeTruthy();
     expect(res._getJSONData()).toStrictEqual(allTodos);
   })
+  it("should handle error in getTodos or app will break", async () => {
+    const errorMessage = {message: "something went wrong"};
+    const rejectedPromise = Promise.reject(errorMessage)
+    TodoModel.find.mockReturnValue(rejectedPromise);
+    // 
+    await TodoController.getTodos(req, res, next);
+    expect(next).toBeCalledWith(errorMessage)
+  });
 })
 
 //
