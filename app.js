8ddfacfed525ed84e715bd669b5b468fbc87aef3
Cloudbop <boppincolin@gmail.com>
2021-04-15 21:30:54 +0100

    rejig app+server setup

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require("express");
+const todoRoutes = require('./routes/todo.routes')
 const app = express();
+
+// middleware - tell express to respond with json
+app.use(express.json())
+// middleware - get, post, put
+app.use("/todos", todoRoutes);
 
 app.get("/", (req, res) => {
   // || res.send for templating
   res.json("Hello world!"); // for REST api 
-});
-
-app.listen(3000, () => {
-  console.log("Server is now running!");
 });
 
 module.exports = app;
