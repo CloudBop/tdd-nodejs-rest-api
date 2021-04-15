b5df834487821f246dab7c18fca7f336e8e6f47d
Cloudbop <boppincolin@gmail.com>
2021-04-15 17:38:44 +0100

    fail: should invoke TodoModel.create() with arguments

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -1,6 +1,7 @@
 const TodoController = require("../../controllers/todo.controller");
 const TodoModel = require("../../model/todo.model");
-
+const httpMocks = require('node-mocks-http');
+const newTodo = require('../mock-data/newTodo.json');
 // mock implementation
 TodoModel.create = jest.fn();
 //
@@ -8,10 +9,20 @@ describe("TodoController.createTodo", () => {
   it("should have a createTodo function", () => {
     expect(typeof TodoController.createTodo).toBe("function");
   });
-  it("should invoke TodoModel.create()", () => {
-    
-    TodoController.createTodo();
-    expect(TodoModel.create).toBeCalled();
+  it("should invoke TodoModel.create() with arguments", () => {
+    //
+    let req,res,next;
+    // mock http requests
+    req = httpMocks.createRequest();
+    res = httpMocks.createResponse();
+    next = null;
+
+    //
+    req.body = newTodo;
+
+    //
+    TodoController.createTodo(req,res,next);
+    expect(TodoModel.create).toBeCalledWith(newTodo);
   });
 
 
