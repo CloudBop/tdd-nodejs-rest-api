fb6b1eda17986063068804161a31fd2d98ea59fc
Cloudbop <boppincolin@gmail.com>
2021-04-15 23:21:59 +0100

    passtest:+middleware -> should 500 as json !html

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ mongodb.connect();
 app.use(express.json())
 // middleware - get, post, put
 app.use("/todos", todoRoutes);
+// 
+app.use((error,req,res,next)=>{
+  //invoked in the next callback
+  res.status(500).json({message:error.message})
+})
 
 app.get("/", (req, res) => {
   // || res.send for templating
