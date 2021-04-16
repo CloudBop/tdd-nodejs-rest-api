af461e886ddd63501bc6486161d142d1a8b65c5c
Cloudbop <boppincolin@gmail.com>
2021-04-16 01:15:20 +0100

    pass:updateTodo return 404 if todo doesnt exist

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -57,10 +57,10 @@ exports.updateTodo = async (req,res,next)=>{
     if(todo){
         res.status(200).json(todo);
     }
-    // else {
-    //   //  res.status(404).json(null); - works too
-    //   res.status(404).send();
-    // }
+    else {
+      //  res.status(404).json(null); - works too
+      res.status(404).send();
+    }
     
   } catch (error) {
     next(error)   
