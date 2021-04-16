fd3778dae216d5e1aa6de308fbf76cca215aac88
Cloudbop <boppincolin@gmail.com>
2021-04-16 02:02:38 +0100

    integration-test:PASS: Delete should deleteTodo

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -7,6 +7,7 @@ router.get("/", todoController.getTodos)
 // req.params.todoId
 router.get("/:todoId", todoController.getTodoById)
 router.put("/:todoId", todoController.updateTodo)
+router.delete("/:todoId", todoController.deleteTodo)
 
 
 module.exports = router
